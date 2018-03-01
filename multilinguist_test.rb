b3da9661b83089645b8a937ba95acc7f80955851
Garfuz <mattpaoli@hotmail.com>
2018-03-01 16:35:58 -0500

    completed

---
+++ multilinguist_test.rb
@@ -0,0 +1,22 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './multilinguist.rb'
+
+class TestMultilinguist < MiniTest::Test
+
+  def setup
+    @language = Multilinguist.new
+  end
+
+  def test_language_in_french
+    assert_equal 'fr', @language.language_in("france")
+  end
+
+  def test_language_in_german
+    assert_equal 've', @language.language_in("germany")
+  end
+
+  def test_language_in_japan
+    refute_equal 'la', @language.language_in("italy")
+  end
+end
